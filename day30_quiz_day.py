0afd32f7468bfea5995903295bf4c77dff041c0b
Lyrakun <273812235+Lyrakunai@users.noreply.github.com>
2026-05-10 17:19:42 +0530

    Add files via upload

---
+++ day30_quiz_day.py
@@ -0,0 +1,36 @@
+# Day 30 - Quiz Day (Loops)
+
+
+# 1. For Loop Tracing
+for i in range(3):
+    print(i)
+
+
+# 2. While Loop Tracing
+i = 1
+while i <= 3:
+    print(i)
+    i += 1
+
+
+# 3. Break Example
+for i in range(5):
+    if i == 3:
+        break
+    print(i)
+
+
+# 4. Continue Example
+for i in range(5):
+    if i == 2:
+        continue
+    print(i)
+
+
+# 5. Nested Loop Output
+for i in range(2):
+    for j in range(2):
+        print(i, j)
+
+
+
